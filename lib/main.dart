885f82cf18e11546e021ed552bc873e08517fd85
adityapuri75 <adityapuri75@gmail.com>
2020-10-02 15:50:20 +0530

    Login done

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,34 @@
-import 'package:flame/game/game.dart';
+import 'package:aspage/Home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import 'game.dart';
+import 'login/authentication/login.dart';
 
-void main() => runApp(MyApp());
+
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      title: 'ASPAGE',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-
-          title: Text('ASPAGE'),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.purple, textSelectionColor: Colors.amber),
+        home: FutureBuilder<FirebaseUser>(
+            future: FirebaseAuth.instance.currentUser(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                FirebaseUser user = snapshot.data;
+                return Home(id: user.uid);
+              } else {
+                return LoginScreen();
+              }
+            }
         ),
-        body: Center(
-          child: RaisedButton(
-            child: Text("PLAY"),
-            onPressed: game,
-            color: Colors.yellow,
-
-          ),
-        ),
-      ),
-    );
+      );
   }
 }
